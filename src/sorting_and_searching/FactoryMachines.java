a842b452636c7543580bbb0c9d15f4373c378615
shviam <shivam.guptaome@gmail.com>
2026-01-31 11:50:03 +0530

    sorting_and_searching

---
+++ src/sorting_and_searching/FactoryMachines.java
@@ -0,0 +1,123 @@
+//package sorting_and_searching;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class FactoryMachines {
+
+    private static final class FastScanner {
+        private final InputStream in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        FastScanner(InputStream is) { this.in = is; }
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') if (c == -1) return -1;
+            int sign = 1;
+            if (c == '-') { sign = -1; c = read(); }
+            int val = c - '0';
+            while ((c = read()) > ' ') val = val * 10 + (c - '0');
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') if (c == -1) return -1;
+            int sign = 1;
+            if (c == '-') { sign = -1; c = read(); }
+            long val = c - '0';
+            while ((c = read()) > ' ') val = val * 10 + (c - '0');
+            return val * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') if (c == -1) return null;
+            StringBuilder sb = new StringBuilder();
+            sb.append((char)c);
+            while ((c = read()) > ' ') sb.append((char)c);
+            return sb.toString();
+        }
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) { long t = a % b; a = b; b = t; }
+        return a;
+    }
+
+    static long modPow(long a, long b, long mod) {
+        long r = 1;
+        while (b > 0) {
+            if ((b & 1) == 1) r = (r * a) % mod;
+            a = (a * a) % mod;
+            b >>= 1;
+        }
+        return r;
+    }
+
+    static FastScanner in = new FastScanner(System.in);
+    static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    static final long INF = (long)1e18;
+    static final int MOD = 1_000_000_007;
+    public static void main(String[] args) throws Exception {
+        solve();
+        out.flush();
+    }
+
+    public static void solve() throws Exception {
+        /*
+            1. Sort
+            2. binary search
+         */
+
+        int n = in.nextInt();
+        int p = in.nextInt();
+
+        int[] m = new int[n];
+        for (int i = 0; i < n; i++){
+            m[i] = in.nextInt();
+        }
+
+
+        Arrays.sort(m);
+        long l = m[0], r = (long)1e19;
+        long ans = r;
+
+        while(l <= r){
+            long mid = l + (r - l) / 2;
+
+            long currentProducts = p;
+            for (int i = 0; i < n && currentProducts > 0; i++){
+                if(mid < m[i]){
+                    break;
+                }else{
+                    long temp = mid / m[i];
+                    currentProducts -= temp;
+                }
+            }
+
+            if(currentProducts <= 0){
+                ans = mid;
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+
+        out.print(ans);
+    }
+}
