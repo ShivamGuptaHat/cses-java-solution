0b6805fe872563bbb44c5315a76a9b65df0495c7
Shivam Gupta <shivam.guptaome@gmail.com>
2026-03-03 22:14:15 +0530

    sorting and searching

---
+++ src/sorting_and_searching/MovieFestivalII.java
@@ -0,0 +1,154 @@
+//package sorting_and_searching;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class MovieFestivalII {
+
+    private static final class FastScanner {
+        private final InputStream in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        FastScanner(InputStream is) {
+            this.in = is;
+        }
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') if (c == -1) return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int val = c - '0';
+            while ((c = read()) > ' ') val = val * 10 + (c - '0');
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') if (c == -1) return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            long val = c - '0';
+            while ((c = read()) > ' ') val = val * 10 + (c - '0');
+            return val * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') if (c == -1) return null;
+            StringBuilder sb = new StringBuilder();
+            sb.append((char) c);
+            while ((c = read()) > ' ') sb.append((char) c);
+            return sb.toString();
+        }
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+
+    static long modPow(long a, long b, long mod) {
+        long r = 1;
+        while (b > 0) {
+            if ((b & 1) == 1) r = (r * a) % mod;
+            a = (a * a) % mod;
+            b >>= 1;
+        }
+        return r;
+    }
+
+    static FastScanner in = new FastScanner(System.in);
+    static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+    static final long INF = (long) 1e18;
+    static final int MOD = 1_000_000_007;
+
+    public static void main(String[] args) throws Exception {
+        solve2();
+        out.flush();
+    }
+
+
+    public static void solve2() throws Exception {
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int[][] movies = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            movies[i][0] = in.nextInt();
+            movies[i][1] = in.nextInt();
+        }
+
+        Arrays.sort(movies, Comparator.comparingInt(a -> a[1]));
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(0, k);
+
+        int cnt = 0;
+        for (int[] movie : movies) {
+            int start = movie[0];
+            int end = movie[1];
+
+            Integer p = map.floorKey(start);
+            if (p != null) {
+                cnt++;
+                if (map.get(p) == 1) {
+                    map.remove(p);
+                } else {
+                    map.put(p, map.get(p) - 1);
+                }
+
+                if (map.containsKey(end)) {
+                    map.put(end, map.get(end) + 1);
+                } else {
+                    map.put(end, 1);
+                }
+            }
+        }
+
+        out.print(cnt);
+    }
+}
+
+
+/*
+
+Why does heap solution fail ?
+P1: 5 (can't watch m1 movie because he is occupied till 5)
+P2: 3 (movie m2 assigned)
+m1:        5 ---- 10
+m2:    3---------------12
+
+Incorrect (using heap):
+P2 <- m1
+Cnt = 1
+
+Correct (using treemap):
+p1 <- m1
+P2 <- m2
+cnt = 2
+
+ */
